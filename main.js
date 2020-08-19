6541e9c852ef8fbf48144761c320fd0fd0372d09
zeel <zeel02@gmail.com>
2020-08-19 15:34:01 -0400

    Switched to using Math.js instead of eval()

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 function _onChangeInputDelta(event) {
 	// Helpers
-	function roll(...args) {
-		return new Roll(...args).roll().total;
-	}
+	
 	let entity = this.entity;
 	let data = this.entity.data;
 	let abilities = this.entity.data.abilities;
 	let attributes = this.entity.data.attributes;
 
+	let scope = {
+		entity, data, abilities, attributes
+	}
+
 	// 
 	const input = event.target;
 	let value = input.value;
 
+	if (value == "") return;
+
 	if (/^[\+\-\/\*]/.test(value)) value = this.current + value;
 	try {
-		let evaluated = Number(eval(value));
+		let evaluated = Number(math.evaluate(value, scope));
+//		let evaluated = Number(eval(value));
 		if (isNaN(evaluated)) throw Error("The expression did not have a numeric result.")
 		input.value = evaluated;
 	}
@@ -26,6 +31,21 @@ function _onChangeInputDelta(event) {
 		return
 	}
 }
+
+Hooks.once("init", () => {
+	if (window.math?.roll) return;
+
+	function roll(args) {
+		let str = typeof args == "object"
+			? args.reduce((s, a) => s + a.toString().replace(/\s/g, ''), "")
+			: args;
+
+		return new Roll(str).roll().total;
+	}
+	roll.rawArgs = true;
+
+	math.import({ roll });
+});
 
 Hooks.on("renderActorSheet", (sheet, element, data) => {
 	const inputs = element.find('[data-dtype="Number"]');
---
+++ readme.md
@@ -0,0 +1,10 @@
+## Input Expressions
+This module makes it possible to enter mathematical expressions into numeric entry fields. This includes the token HUD, as well as many inputs on character and item sheets. These expressions support basic arithmatic, but also support anything from the [Math.js](https://mathjs.org/) library.
+
+![Example GIF](Expressions_005.gif)
+
+Additionally, there is a `roll()` helper function, which can be used to roll dice and get the result immediately.
+```
+roll(3d6)
+```
+When working with a token/actor, access to the `attributes` and `abilities` objects are also available. For more advanced users, `entity` allows access to the entire Entity, and `data` is a shortcut to its data property.
