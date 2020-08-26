76ab2637a899a42ae1eb652cc581d5f0780fc598
zeel <zeel02@gmail.com>
2020-08-26 01:04:45 -0400

    Removed stuff that wans't working out.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,5 @@
 const Gulp = require('gulp');
 const zip = require('gulp-zip');
-var git = require('gulp-git');
-
-function defaultTask(cb) {
-	// place code for your default task here
-	cb();
-}
 
 function createRelease(cb) {
 	return Gulp.src([
@@ -16,11 +10,6 @@ function createRelease(cb) {
 	]).pipe(zip("module.zip"))
 	  .pipe(Gulp.dest("./"));
 }
-async function commitChanges(cb) {
-	let manifest = await require("module.json");
-	return Gulp.src("./*")
-		.pipe(git.commit())
-}
 
 exports.release = createRelease;
-exports.default = defaultTask;
+exports.default = createRelease;
