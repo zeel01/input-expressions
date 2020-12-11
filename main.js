ca10a3e1e678cf8019901db86684f0debfd6a428
zeel <zeel02@gmail.com>
2020-12-11 16:35:10 -0500

    Fixed issue introduced in 0.7.x which broke the HUD

--- main.js
+++ main.js
@@ -8,50 +8,59 @@ class TextInputAdapter extends InputAdapter {
 	}
 }
 
-Hooks.once("init", inputExprInitHandler);
-
-function sheetHook(sheet, element, sheetdata, appdata) {
-	element.find('[data-dtype="Number"]')
-	.off("change").on("change", (event) => {
-		const current = getProperty(sheetdata, event.target.name);
-		inputExpression(
-			new TextInputAdapter(event.target), current, {
-				entity: sheetdata,
-				data: appdata,
-				event,
-				actor: sheet.actor
-			}
-		);
-	});
-}
-
-Hooks.on("renderActorSheet", (sheet, element, data) => sheetHook(sheet, element, sheet.actor.data, data));
-Hooks.on("renderItemSheet", (sheet, element, data) => sheetHook(sheet, element, sheet.item.data, data));
-
-Hooks.on("renderTokenHUD", (hud, element, data) => {
-	element.find(".attribute input")
-	.off("change").on("change", (event) => {
+class InputExpressionsHandlers {
+	static sheetHook(sheet, element, sheetdata, appdata) {
+		element.find('[data-dtype="Number"]')
+		.off("change").on("change", (event) => {
+			const current = getProperty(sheetdata, event.target.name);
+			inputExpression(
+				new TextInputAdapter(event.target), current, {
+					entity: sheetdata,
+					data: appdata,
+					event,
+					actor: sheet.actor
+				}
+			);
+		});
+	}
+	
+	static hudEventHandler(event, hud, data, obj) {
 		const input = event.target
 		const bar = input.dataset.bar;
 		let current;
 		if (bar) {
-			current = getProperty(hud.object.actor.data.data, hud.object.data[bar].attribute);
+			current = getProperty(obj.actor.data.data, obj.data[bar].attribute);
 			// Sometimes the attribute is just a value, sometimes it's an object with value/max
 			if (typeof current == "object") {
 				current = current.value;
 			}
 		}
-		else current = hud.object.data[input.name];
+		else current = obj.data[input.name];
 		
 		inputExpression(
 			new TextInputAdapter(input), current, { 
-				entity: hud.object.actor.data,
-				actor: hud.object.actor,
+				entity: obj.actor.data,
+				actor: obj.actor,
 				data, event, negativesAreDelta: true
 			}
 		);
 
 		hud._onAttributeUpdate(event);
+	}
+}
+
+Hooks.once("init", inputExprInitHandler);
+
+Hooks.on("renderActorSheet", (sheet, element, data) => InputExpressionsHandlers.sheetHook(sheet, element, sheet.actor.data, data));
+Hooks.on("renderItemSheet", (sheet, element, data) => InputExpressionsHandlers.sheetHook(sheet, element, sheet.item.data, data));
+
+Hooks.on("renderTokenHUD", (hud, element, data) => {
+	const obj = hud.object;
+	element.find(".attribute input")
+	.off("change").on("change", (event) => InputExpressionsHandlers.hudEventHandler(event, hud, data, obj))
+	.off("keydown").on("keydown", (event) => {
+		const code = game.keyboard.getKey(event);
+    	if (code === "Enter") return InputExpressionsHandlers.hudEventHandler(event, hud, data, obj);
 	});
 });
 
